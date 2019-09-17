2b505c65bd011785bef23d5f9f571a013ceb0523
Martin Fluor <martin.fluor@wuerth-itensis.com>
2019-09-17 16:04:33 +0200

    Ongoing development

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-  var SlackBot = require("slackbots");
-  var schedule = require('node-schedule');
-  var express = require("express");
-  var app = express();
-  var channel = "general";
-  var port = process.env.PORT || 8080;
+const SlackBot = require('slackbots');
+const schedule = require('node-schedule');
+const fetch = require('node-fetch');
+const auth = require('./auth');
+const channel = 'test2';
 
-  var server = app.listen(port,function() {
-  console.log("app running on port 8080"); });
+const bot = new SlackBot({
+	token: auth.slackToken,
+	name: 'course'
+});
 
-  var bot = new SlackBot({
-      token: 'xoxb-604611695190-762654333568-pL9ZOjjHRfkvqDbFkvOIXK88',
-      name: "Beer"
-  });
+bot.on('start', function () {
 
+	const getUpcomingCalanderItems = () => {
+		return fetch(`https://moodle.hsr.ch/webservice/rest/server.php?wstoken=${auth.moodleToken}&wsfunction=core_calendar_get_calendar_upcoming_view&moodlewsrestformat=json`)
+		.then(res => res.json());
+	};
 
-  bot.on("start", function() {
-    var arr = [ "I work until beer o'clock. Grab some?",
-                "Should we grab some beer?  A)Yes   B)A   C)B",
-                "Who needs a MAS when there's beer. Let's get one.",
-                "Stop trying to make everyone happy. You're not a beer.",
-                "Save water. Drink beer. Do you wanna be a water saver?",
-                "Anytime you're sad, remember this: Beer.",
-                "So many beers, so little time. Waste your time today!",
-                "I got 99 problems and beer solves all of them.",
-                "Shhhh.... yep I hear... A beer calling me!",
-                "Beauty is in the eye of the beer holder. Let's enjoy this beauty today!",
-                "Beer! Because you can't drink bacon (or quinoa for the vegans).",
-                "Wish you were beer!" ];
+	const scheduler = async () => {
+		let calendarItems = await getUpcomingCalanderItems();
 
-                //30 16 * * 5
-                var scheduler = schedule.scheduleJob('45 11 * * 2', function(){
-                  var random = arr[Math.floor(Math.random() * arr.length)];
-                  bot.postMessageToChannel(channel, random);
-                });
+		let msg = '';
+		// todo: filter only the next two days
+		for (let event of calendarItems.events) {
+			let date = new Date(0);
+			date.setUTCSeconds(event.timestart);
+			msg += `${date.toLocaleString()}: ${event.name} \n`;
+		}
+		console.log(msg);
+		bot.postMessageToChannel(channel, msg);
+	};
 
+	scheduler().then(() => console.log('done'));
 
-  });
+	// todo: write /course in chat and get the infos for next week
+	//30 16 * * 5
+	/*schedule.scheduleJob('30 09 * * 4', function () {
+		var random = arr[Math.floor(Math.random() * arr.length)];
+		bot.postMessageToChannel(channel, random);
+	});*/
+
+});
