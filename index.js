8d3735648b6fed84af491f130a1f61a989f350af
Martin Fluor <martin.fluor@wuerth-itensis.com>
2019-09-18 13:51:36 +0200

    Ongoing Development
    Ongoing Development
    
    Ongoing Development
    
    Ongoing Development
    
    Ongoing Development
    
    Ongoing Development

--- index.js
+++ index.js
@@ -1,42 +1,102 @@
 const SlackBot = require('slackbots');
 const schedule = require('node-schedule');
+const moment = require('moment');
 const fetch = require('node-fetch');
-const auth = require('./auth');
-const channel = 'test2';
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const channel = 'general';
+const port = process.env.PORT || 8080;
 
 const bot = new SlackBot({
-	token: auth.slackToken,
+	token: process.env.SLACK_TOKEN,
 	name: 'course'
 });
 
+console.log(process.env.SLACK_TOKEN)
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+
 bot.on('start', function () {
+	console.log('bot started');
+	console.log('local time:', moment().format());
 
 	const getUpcomingCalanderItems = () => {
-		return fetch(`https://moodle.hsr.ch/webservice/rest/server.php?wstoken=${auth.moodleToken}&wsfunction=core_calendar_get_calendar_upcoming_view&moodlewsrestformat=json`)
+		return fetch(`https://moodle.hsr.ch/webservice/rest/server.php?wstoken=${process.env.MOODLE_TOKEN}&wsfunction=core_calendar_get_calendar_upcoming_view&moodlewsrestformat=json`)
 		.then(res => res.json());
+	};
+
+	const convertDates = events => {
+		return events.map(event => {
+			let date = new Date(0);
+			date.setUTCSeconds(event.timestart);
+			event.date = date;
+			let endDate = new Date(0);
+			endDate.setUTCSeconds(event.timestart + event.timeduration);
+			event.endDate = endDate;
+			return event;
+		});
+	};
+
+	const filterOnlyNextEvent = events => {
+		let byDate = {};
+		for (let event of events) {
+			let dateString = event.date.toLocaleDateString();
+			if (byDate[dateString]) {
+				byDate[dateString].push(event);
+			} else {
+				byDate[dateString] = [event];
+			}
+		}
+
+		return Object.keys(byDate).reduce((arr, key, index, array) => {
+			if (index < 2) {
+				let obj = {};
+				obj[key] = byDate[key];
+				arr.push(obj);
+			}
+			return arr;
+		}, []);
+	};
+
+	const getAnnouncement = dates => {
+		return dates.map(date => {
+			return date[Object.keys(date)[0]].map(event => {
+				let formattedDate = moment(event.date).format('dddd, D. MMMM ');
+				let startTime = moment(event.date).format('HH:mm');
+				let endTime = moment(event.endDate).format('HH:mm');
+				console.log(event.date);
+				return `:unicorn_face: *${formattedDate}*\n:clock430: ${startTime} - ${endTime}\n:money_with_wings: ${event.name} ${event.description.replace('<br />', '')}\n\n`;
+			}).join('\n');
+		}).join('\n');
 	};
 
 	const scheduler = async () => {
 		let calendarItems = await getUpcomingCalanderItems();
+		let events = convertDates(calendarItems.events);
 
-		let msg = '';
-		// todo: filter only the next two days
-		for (let event of calendarItems.events) {
-			let date = new Date(0);
-			date.setUTCSeconds(event.timestart);
-			msg += `${date.toLocaleString()}: ${event.name} \n`;
+		let today = moment();
+		let firstDate = moment(events[0].date);
+
+		// only send if the next date is in this week!
+		if (firstDate.diff(today, 'days') < 5) {
+			let relevantEvents = filterOnlyNextEvent(events);
+			let announcement = `Good news everyone! Here's our upcoming classes:\n${getAnnouncement(relevantEvents)}`;
+			console.log(announcement);
+			bot.postMessageToChannel(channel, announcement);
 		}
-		console.log(msg);
-		bot.postMessageToChannel(channel, msg);
+
 	};
 
-	scheduler().then(() => console.log('done'));
-
 	// todo: write /course in chat and get the infos for next week
-	//30 16 * * 5
-	/*schedule.scheduleJob('30 09 * * 4', function () {
-		var random = arr[Math.floor(Math.random() * arr.length)];
-		bot.postMessageToChannel(channel, random);
-	});*/
+	// every thursday at half past 9 - 30 09 * * 4
+	schedule.scheduleJob('30 09 * * 4', function () {
+		scheduler().then(() => console.log('sent schedule'));
+	});
 
+}).on('error', (err) => {
+	console.log('Error: ' + err.message);
 });
